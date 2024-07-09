be879be5b0ad07169295985cb129ab223bc31579
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-09 16:54:42 +0530

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,49 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DevOverflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures and more.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">Hlo Everyone</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
